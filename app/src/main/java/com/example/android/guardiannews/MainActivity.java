5a5a5048f1f382896bdb579690e10bcb0012a299
RogerVW <roger.vanwyk@gmail.com>
2021-10-10 23:23:29 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/guardiannews/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.android.guardiannews;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/* CREATED BY ROGER VW AS PART OF ABND(UDACITY.COM) BY GOOGLE */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
